e46cda6bc49d65007b845064090db87c981f706b
Ali Rehman <alee.rehmaann@gmail.com>
2024-08-12 11:38:02 +0500

    Update segformer.py

--- edgeseg/model_zoo/segformer.py
+++ edgeseg/model_zoo/segformer.py
@@ -3,16 +3,16 @@ from transformers import SegformerFeatureExtractor, SegformerForSemanticSegmenta
 
 def Segformer(name="b1"):
     if name=="b0":
-        model = SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b0-finetuned-cityscapes-1024-1024")
+        return SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b0-finetuned-cityscapes-1024-1024")
     elif name=="b1":
-        model=SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b1-finetuned-cityscapes-1024-1024")
+       return SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b1-finetuned-cityscapes-1024-1024")
     elif name=="b2":
-        model=SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b2-finetuned-cityscapes-1024-1024")
+        return SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b2-finetuned-cityscapes-1024-1024")
     elif name=="b3":
-        model=SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b3-finetuned-cityscapes-1024-1024")
+        return SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b3-finetuned-cityscapes-1024-1024")
     elif name=="b4":
-        model=SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b4-finetuned-cityscapes-1024-1024")
+        return SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b4-finetuned-cityscapes-1024-1024")
     elif name=="b5":
-        model=SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b5-finetuned-cityscapes-1024-1024")
+        return SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b5-finetuned-cityscapes-1024-1024")
     else:
         print("Invalid Mdel Name ")
